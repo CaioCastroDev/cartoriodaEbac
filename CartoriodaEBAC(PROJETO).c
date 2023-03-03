6c35534016fc40f627153d41a8aa237eedc5eefa
CaioCastroDev <126790635+CaioCastroDev@users.noreply.github.com>
2023-03-03 11:26:45 -0300

    Criação da tela login

--- CartoriodaEBAC(PROJETO).c
+++ CartoriodaEBAC(PROJETO).c
@@ -116,51 +116,67 @@ int deletar() //Função resposável por deletar nomes do banco de dados
 
 int main ()
 {
-	int opcao=0; //Definindo variaveis
-	
+	int opcao=0; //Definindo variaveis    
 	int loop=1;
+	char senhadigitada[10]="a";
+	int comp;
 	
-	for(loop=1;loop=1;)
-	{
-		system("cls");
-		
-		setlocale (LC_ALL, "Portuguese"); // Definindo a linguagem
+	setlocale (LC_ALL, "Portuguese"); // Definindo a linguagem
 	
-		printf("### Cartório da EBAC ###\n\n"); //Inicio do menu
-		printf("Escolha a opção desejada do menu:\n\n");
-		printf("\t1 - Registrar nomes\n");
-		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n");
-		printf("\t4 - Sair do sistema\n\n");
- 		printf("Opção: "); //Fim do menu
- 	
-		scanf("%d", &opcao); //Armazenando a escolha do usuário
-		
-		system("cls"); //responsável por limpar as telas 
+	printf("### Cartório da EBAC ###\n\n");
+	printf("Login de administrador:\n\nDigite a sua senha: ");
+	scanf("%s",senhadigitada);
 
-		switch	(opcao) //chamada de funções
-		{
-			case 1:
-			registro();
-			break;
+	comp = strcmp(senhadigitada, "admin");
+
+	if(comp == 0)
+	{	
+		system("cls");	
+		for(loop=1;loop=1;)
+		{						
+			system("cls");
+					
+			setlocale (LC_ALL, "Portuguese"); // Definindo a linguagem
+		
+			printf("### Cartório da EBAC ###\n\n"); //Inicio do menu
+			printf("Escolha a opção desejada do menu:\n\n");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar nomes\n");
+			printf("\t3 - Deletar nomes\n");
+			printf("\t4 - Sair do sistema\n\n");
+ 			printf("Opção: "); //Fim do menu
+ 		
+			scanf("%d", &opcao); //Armazenando a escolha do usuário
 			
-			case 2:
-			consulta();
-			break;
-			
-			case 3:
-			deletar();
-			break;
-			
-			case 4:
-			printf("Obrigado e até breve!\n");
-			return 0;
-						
-			default:
-			printf("Essa opção não está disponível\n");
-			system("pause");
-			break;
+			system("cls"); //responsável por limpar as telas 	
+
+			switch	(opcao) //chamada de funções
+			{
+				case 1:
+				registro();
+				break;
+				
+				case 2:
+				consulta();
+				break;
+				
+				case 3:
+				deletar();
+				break;
+				
+				case 4:
+				printf("Obrigado e até breve!\n");
+				return 0;
+							
+				default:
+				printf("Essa opção não está disponível\n");
+				system("pause");
+				break;		
+			}	
 		}
 	}
+	else
+		printf("\n\nSenha incorreta.");		
 }
+
 
